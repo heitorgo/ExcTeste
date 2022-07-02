50d957e49ce8f2cc88a7ff4316400fa5428ad57b
heitorgon <heitor.2000goncalves@gmail.com>
2022-07-02 11:31:18 -0300

    resolucao

---
+++ Principal.java
@@ -0,0 +1,17 @@
+package exercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Integer indice = 13;
+		Integer soma = 0;
+		Integer k = 0;
+		while(k<indice) {
+			k=k+1;
+			soma=soma+k;
+		}
+		System.out.println("O valor de soma é "+soma);
+
+	}
+
+}
